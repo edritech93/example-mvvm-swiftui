f2022757c5ed70aefb7b12f87efcd3be0808a350
Yudi Alviska <yudi.a@weefer.co.id>
2024-05-21 14:06:34 +0700

    Initial Commit

---
+++ MVVMSwiftUIApp/MVVMSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMSwiftUIAppApp.swift
+//  MVVMSwiftUIApp
+//
+//  Created by Yudi Edri Alviska on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MVVMSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
